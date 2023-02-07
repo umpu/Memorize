ad78127f41a4b77e0d57471d31e6d0cc550cc7e0
umpu <pavfE0-huwpup-mavcyn>
2023-02-07 19:20:03 +0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Andrew Gorbunov on 07.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
