fd8748d87d3b4c42e15dc44d3445d0396a1dbbbe
umpu <pavfE0-huwpup-mavcyn>
2023-02-07 19:21:57 +0500

    CS193p. Lecture 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,26 +1,100 @@
-//
-//  ContentView.swift
-//  Memorize
-//
-//  Created by Andrew Gorbunov on 07.02.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    var activity = ["⚽️", "🏀", "🏈", "⚾️", "🥎", "🎾", "🏐", "🏉", "🥏", "🎱", "🪀", "🏓", "🏸", "🏒", "🏑", "🥍", "🏏", "🪃", "🥅", "⛳️"]
+    var objects = ["⌚️", "📱", "💻", "⌨️", "🖥", "🖨", "🖱", "🖲", "🕹", "🗜", "💽", "💾", "💿", "📼", "📷", "📹", "🎥", "📽", "🎞", "📞", "☎"]
+    var flags = ["🏳️", "🏴", "🏴‍☠️", "🏁", "🚩", "🏳️‍🌈", "🏳️‍⚧️", "🇺🇳", "🇦🇫", "🇦🇽", "🇦🇱", "🇩🇿", "🇦🇸", "🇦🇩", "🇦🇴", "🇦🇮", "🇦🇶", "🇦🇬", "🇦🇷", "🇦🇲"]
+    
+    var emojis: [String] {
+        return activity
+    }
+    
+    @State var emojiCount = 16
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Memorize!")
+                .font(.largeTitle).bold()
+            
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                        .aspectRatio(2/3, contentMode: .fit)
+                }
+            }
+            .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+            
+            Spacer()
+            
+            HStack {
+                remove
+                Spacer()
+                shuffle
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
-        .padding()
+        .padding(.horizontal)
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.square")
+        }
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.square")
+        }
+    }
+    
+    var shuffle: some View {
+        Button {
+            emojiCount = 4
+        } label: {
+            Text("Reset")
+        }
+    }
+    
+    struct CardView: View {
+        var content: String
+        @State private var isActive = true
+        
+        var body: some View {
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: 20)
+                
+                if isActive {
+                    shape.fill().foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    shape.strokeBorder(lineWidth: 3)
+                    Text(content).font(.largeTitle)
+                } else {
+                    shape.fill()
+                }
+            }
+            .onTapGesture {
+                withAnimation {
+                    isActive = !isActive
+                }
+            }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+            //.preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+        }
     }
 }
